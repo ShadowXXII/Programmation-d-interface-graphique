c5759fa799ee725561017d455bd7831dadf025a5
ShadowXXII <94879756+ShadowXXII@users.noreply.github.com>
2025-08-25 11:15:55 -0400

    Exercices cours 1

---
+++ Exercices/Exo 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre chiffre: ");
+            int chiffre = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                int chiffreProduit = chiffre * i;
+                Console.WriteLine(chiffre + " x " + i + " = " + chiffreProduit);
+            }
+            
+
+            Console.Read();
+        }
+    }
+}
---
+++ Exercices/Exo 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean tailleEntre2et20 = true;
+            while (tailleEntre2et20 == true)
+            {
+                Console.WriteLine("Entrez la taille du tableau: ");
+                int taille = Convert.ToInt32(Console.ReadLine());
+                if (taille >= 2 && taille <= 20)
+                {
+                    tailleEntre2et20 = false;
+                    int nombrePlusGrand = int.MinValue;
+                    int nombrePlusPetit = int.MaxValue;
+                    int[] nombres = new int[taille];
+                    int i = 0;
+                    foreach (int nombre in nombres)
+                    {
+                        Console.WriteLine("Entrez votre chiffre: ");
+                        int chiffre = Convert.ToInt32(Console.ReadLine());
+                        nombres[i] = chiffre;
+
+                        if (nombres[i] < nombrePlusPetit)
+                        {
+                            nombrePlusPetit = nombres[i];
+                        }
+
+                        if (nombres[i] > nombrePlusGrand)
+                        {
+                            nombrePlusGrand = nombres[i];
+                        }
+                        i++;
+                    }
+                    Console.WriteLine("Le nombre le plus grand est: " + nombrePlusGrand + ", le nombre le plus petit est: " + nombrePlusPetit + ", dans le tableux: ");
+                    foreach (int nombre in nombres)
+                    {
+                        Console.WriteLine(nombre);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("La taille est invalide ");
+                    tailleEntre2et20 = true;
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
